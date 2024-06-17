7d9054e4ae5e2144f5b70aadb118b6347c473b29
Jeizer-Valarezo <nakellava@hotmail.com>
2024-06-17 23:13:24 +0200

    2 horas trabajo intenso

--- VidaRPG/src/Componentes/data/recompensasData.js
+++ VidaRPG/src/Componentes/data/recompensasData.js
@@ -1,9 +1,8 @@
 const recompensas = [
-    { nombre: "1 Pajilla", precio: 7 },
     { nombre: "Videojuegos 1.5 horas ", precio: 2 },
-    { nombre: "YouTube", precio: 3 },
+    { nombre: "YouTube 1 hora", precio: 3 },
     { nombre: "Procrastinar 1 hora", precio: 1 },
-    { nombre: "Sin restricciones", precio: 18 },
+    { nombre: "Sin restricciones", precio: 13 },
   ];
   
   export default recompensas;
