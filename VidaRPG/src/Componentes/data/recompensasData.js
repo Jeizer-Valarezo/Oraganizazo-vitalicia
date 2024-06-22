a35c3d147c659d59ce1f60ca3abb93e0f46f0760
Jeizer-Valarezo <nakellava@hotmail.com>
2024-06-22 21:18:10 +0200

    El señor de la noche

--- VidaRPG/src/Componentes/data/recompensasData.js
+++ VidaRPG/src/Componentes/data/recompensasData.js
@@ -2,6 +2,7 @@ const recompensas = [
     { nombre: "Videojuegos 1.5 horas ", precio: 2 },
     { nombre: "YouTube 1 hora", precio: 3 },
     { nombre: "Procrastinar 1 hora", precio: 1 },
+    { nombre: "1 Pajilla", precio: 7 },
     { nombre: "Sin restricciones", precio: 13 },
   ];
   
